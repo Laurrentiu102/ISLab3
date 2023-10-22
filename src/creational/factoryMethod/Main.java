c1b7422f6b223912c5b4e30a4986181c66451b1f
Laurentiu Neghina <neghinalaurentiu09@gmail.com>
2023-10-22 18:10:55 +0300

    structural

--- src/creational/factoryMethod/Main.java
+++ src/creational/factoryMethod/Main.java
@@ -8,23 +8,14 @@ public class Main {
         runBusinessLogic();
     }
 
-    /**
-     * The concrete factory is usually chosen depending on configuration or
-     * environment options.
-     */
     static void configure() {
-        if (System.getProperty("os.name").equals("Windows 10")) {
+        if (System.getProperty("os.name").equals("Windows 11")) {
             dialog = new WindowsDialog();
         } else {
             dialog = new HtmlDialog();
         }
     }
 
-    /**
-     * All of the client code should work with factories and products through
-     * abstract interfaces. This way it does not care which factory it works
-     * with and what kind of product it returns.
-     */
     static void runBusinessLogic() {
         dialog.renderWindow();
     }
